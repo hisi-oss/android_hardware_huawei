e03094c1d8414afefd40525444c4c27a0a6374c1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-14 15:30:52 +0200

    libinit_hisi: Add generic libinit
    Change-Id: If052e2e774ab39619d06731b18681b6964b088e3

---
+++ libinit_hisi/include/libinit_utils.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <string>
+
+void property_override(std::string prop, std::string value, bool add = true);
---
+++ libinit_hisi/init_hisi.cpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "vendor_init.h"
+
+void vendor_load_properties() {}
