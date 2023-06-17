d6990098f78575e5033bf8988edeff4f5db1b076
Tim Zimmermann <tim@linux4.de>
2023-06-17 14:18:24 +0000

    mkbootimg: Include boot_ramdisk and recovery_vendor to ota
    Change-Id: I045341bf86e24baf729dae5789c0aaf237ac36fb

---
+++ releasetools/releasetools.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import common
+import re
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def AddImage(info, dir, basename, dest):
+  data = info.input_zip.read(dir + "/" + basename)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+  AddImage(info, "RADIO", "boot_ramdisk.img", "/dev/block/by-name/ramdisk")
+  AddImage(info, "RADIO", "recovery_vendor.img", "/dev/block/by-name/recovery_vendor")
+  return
