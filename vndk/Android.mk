2f6b45db031d5e004e2c44ce4d0420bdbf22c5ed
A2L5E0X1 <alexed25@protonmail.com>
2024-02-04 22:01:44 +0100

    vndk: Add libjsoncpp
    * required by kirin970's camera stack
    
    Change-Id: I191d31ebbbc3aa778349d71970493b71067114b6

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,6 +1,19 @@
 LOCAL_PATH := prebuilts/vndk
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libjsoncpp-v29
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_arm := v29/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libjsoncpp.so
+LOCAL_SRC_FILES_arm64 := v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libjsoncpp.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+LOCAL_MODULE_TAGS := optional
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libprotobuf-cpp-full-v29
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_arm := v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so
