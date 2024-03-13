a6141a4508def3964ed50ddee8c7ec2bd9039258
Giovanni Ricca <giovanniricca@proton.me>
2024-03-13 22:07:01 +0100

    hisi_init: Add generic `hisi_init`
    Change-Id: Idb7373b4bd90122258e2ec3a0e2fb5d280fad775

---
+++ hisi_init/hisi_init.cpp
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "hisi_init"
+
+#include <android-base/logging.h>
+
+int main() {
+}
