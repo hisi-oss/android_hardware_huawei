dc3950bd20f4e1a13cdeb3f6ed28f192b7852bbc
R0rt1z2 <roger@r0rt1z2.com>
2024-03-13 22:20:35 +0100

    hisi_init: Introduce `hisi_nve`
    * This is done in stock by the `mac_addr_normalization` binary
      in a non-treble compliant way. The stock binary uses the NVE
      kernel API interface which relies on `libnvme.so` & co.
    
    * We can get rid of said binary and fix treble compliance with
      this new implementation.
    
    Change-Id: I63a6d233fc5e34f7edb796bff31366c8b319de0b

--- hisi_init/hisi_init.cpp
+++ hisi_init/hisi_init.cpp
@@ -6,10 +6,13 @@
 #define LOG_TAG "hisi_init"
 
 #include "include/hisi_connectivity.h"
+#include "include/hisi_nve.h"
 
 #include <android-base/logging.h>
 
 int main() {
     LOG(INFO) << "Running hisi_connectivity";
     load_hisi_connectivity();
+    LOG(INFO) << "Running hisi_nve";
+    load_hisi_nve();
 }
