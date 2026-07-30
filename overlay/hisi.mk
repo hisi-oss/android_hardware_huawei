dee8e29599160235e2ff83eb5e22b7f98cdfccb9
Giovanni Ricca <giovanniricca@proton.me>
2026-07-30 18:35:05 +0200

    overlay: Introduce common Hisi overlays
    Change-Id: Ideebe024e1f1806287edd9c433d72f0d0a91a6f8

---
+++ overlay/hisi.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    HisiFrameworkOverlay \
+    HisiSystemUIOverlay \
+    HisiWifiOverlay
